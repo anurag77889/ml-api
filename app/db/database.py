4d1b2173f712232a4239ca5e904da9c9ae7e628c
Anurag Swarnakar <anuragswarnakar626@gmail.com>
2026-02-14 12:59:44 +0530

    "DB Per Request"

--- app/db/database.py
+++ app/db/database.py
@@ -1,24 +1,33 @@
 import sqlite3
 
 # Create SQLite database connection
-conn = sqlite3.connect("predictions.db", check_same_thread=False)
-cursor = conn.cursor()
+def get_db_connection():
+    conn = sqlite3.connect("predictions.db")
+    return conn 
 
-# Create table if not exists
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS predictions (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        age INTEGER,
-        salary REAL,
-        prediction INTEGER   
-   )
-""")
-conn.commit()
+def create_table():
+    conn = get_db_connection()
+    cursor = conn.cursor()      
+
+    # Create table if not exists
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS predictions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            age INTEGER,
+            salary REAL,
+            prediction INTEGER   
+        )
+    """)
+    conn.commit()
+    conn.close()
 
 def insert_prediction(age, salary, prediction):
+    conn = get_db_connection()
+    cursor = conn.cursor()
     # Insert into DB
-        cursor.execute(
-            "INSERT INTO predictions (age, salary, prediction) VALUES (?, ?, ?)",
-            (age, salary, int(prediction))
-        )
-        conn.commit()
+    cursor.execute(
+        "INSERT INTO predictions (age, salary, prediction) VALUES (?, ?, ?)",
+        (age, salary, int(prediction))
+    )
+    conn.commit()
+    conn.close()
